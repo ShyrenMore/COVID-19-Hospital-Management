cc5c8548eb26796a5db9f40285abc3f85502b314
Shyren <shyren.more30@gmail.com>
2020-11-19 09:38:35 +0530

    setup messages framework

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import os.path
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'), )
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
