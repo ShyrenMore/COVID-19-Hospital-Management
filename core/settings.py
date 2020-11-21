b09a5ad08cdd41c331184366350e8e2014ff2504
Shyren <shyren.more30@gmail.com>
2020-11-21 13:03:47 +0530

    Add symptons field in patient model

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'django_filters',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
--- main/models.py
+++ main/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
+from multiselectfield import MultiSelectField
 # Create your models here.
-
-
 
 class Patient(models.Model):
     name = models.CharField(max_length=50)
@@ -10,7 +8,20 @@ class Patient(models.Model):
     patient_relative_name = models.CharField(max_length=50, null=True)
     patient_relative_contact = models.CharField(max_length=15, null=True)
     address = models.TextField()
-    symptoms = models.TextField(null=True)
+    SYMPTOMS = (
+        ('Fever', 'Fever'),
+        ('Dry cough', 'Dry cough'),
+        ('Tiredness', 'Tiredness'),
+        ('Aches and pains', 'Aches and pains'),
+        ('Sore throat', 'Sore throat'),
+        ('Diarrhoea', 'Diarrhoea'),
+        ('Loss of taste or smell', 'Loss of taste or smell'),
+        ('Difficulty in breathing or shortness of breath', 'Difficulty in breathing or shortness of breath'),
+        ('Chest pain or pressure', 'Chest pain or pressure'),
+        ('Loss of speech or movement', 'Loss of speech or movement'),
+    )
+
+    symptoms = MultiSelectField(choices=SYMPTOMS, null=True)
     prior_ailments = models.TextField()
     bed_num = models.ForeignKey("Bed", on_delete=models.CASCADE)
     dob = models.DateField(null=True)
