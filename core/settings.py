97a5cd599a58db17eee9875187828937bb474864
Shyren <shyren.more30@gmail.com>
2020-11-22 08:48:02 +0530

    Deployment changes

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '6=drjci9-24*%p-ru1sbqbl0$$!b)_q&_cu5z26y7wk+wk9g!^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hospital-management-covid19.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
